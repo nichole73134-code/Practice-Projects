c142d8d064e73b571284732d8ab8087609ca2037
nichole73134-code <nichole73134@gmail.com>
2026-09-21 15:26:10 -0400

    Add files via upload

---
+++ Lesson 12 3a.sql
@@ -0,0 +1,28 @@
+---Create new table 
+CREATE TABLE product (
+    product_code INTEGER PRIMARY KEY,
+    product_name TEXT,
+    price REAL,
+    stock INTEGER,
+    category TEXT
+);
+
+CREATE TABLE customer (
+    customer_id INTEGER PRIMARY KEY,
+    customer_name TEXT,
+    customer_location TEXT,
+    customer_phone_number TEXT
+);
+
+CREATE TABLE sales (
+    date TEXT,
+    order_number INTEGER,
+    product_code INTEGER,
+    product_name TEXT,
+    quantity INTEGER,
+    price REAL
+);
+
+--Write a query to display the orderid, customer id, order date, price, and quantity columns of the sales table---
+select *
+from sales;
---
+++ Lesson 12 3c product.sql
@@ -0,0 +1,37 @@
+---Create new table 
+CREATE TABLE product (
+    product_code INTEGER PRIMARY KEY,
+    product_name TEXT,
+    price REAL,
+    stock INTEGER,
+    category TEXT
+);
+
+CREATE TABLE customer (
+    customer_id INTEGER PRIMARY KEY,
+    customer_name TEXT,
+    customer_location TEXT,
+    customer_phone_number TEXT
+);
+
+CREATE TABLE sales (
+    date TEXT,
+    order_number INTEGER,
+    product_code INTEGER,
+    product_name TEXT,
+    quantity INTEGER,
+    price REAL
+);
+
+--Write a query to display the orderid, customer id, order date, price, and quantity columns of the sales table---
+select *
+from sales;
+
+---Show details from the product table what the category is stationary--
+select category stationary
+from product;
+
+
+---Display the unique categories in the product table--
+select distinct category
+from product;
